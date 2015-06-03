210007dc019418427221440d26db4316b697f69b
piers7 <male@piers7.com>
2015-06-03 19:53:09 +0800

    change folder for SWIG generation

--- csharp/generateSWIGWrapper.sh
+++ csharp/generateSWIGWrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir src -p
-swig -outdir src -namespace rpi_ws281x -dllimport libws2811 -module ws281x -csharp ../ws2811.h
+mkdir swigSrc -p
+swig -outdir swigSrc -namespace rpi_ws281x -dllimport libws2811 -module ws281x -csharp ../ws2811.h
