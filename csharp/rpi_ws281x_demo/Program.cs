461516fdb2a952cf226a94e821a0166578720975
piers7 <male@NOSPAM.piers7.com>
2015-06-03 19:50:06 +0800

    Simple .net wrapper class over raw p/invoke signatures

---
+++ csharp/rpi_ws281x_demo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace rpi_ws281x
+{
+    class Program
+    {
+        const int TARGET_FREQ                              = 800000;
+        const int GPIO_PIN                                 = 18;
+        const int DMA                                      = 5;
+
+        const int WIDTH                                    = 8;
+        const int HEIGHT                                   = 8;
+        const int LED_COUNT                                = (WIDTH * HEIGHT);
+
+        static volatile int _cancelRequested;
+
+        static int Main(string[] args)
+        {
+            Console.TreatControlCAsInput = true;
+            Console.CancelKeyPress += (o, e) => _cancelRequested = 1;
+
+            using (var client = Ws281xClient.Create(LED_COUNT, GPIO_PIN))
+            {
+                for (int i = 0; i < LED_COUNT; i++)
+                {
+                    if (_cancelRequested > 0)
+                        return 0;
+
+                    Console.WriteLine("Set LED {0}", i);
+                    client.SetPixelColor(i, 0x00FF0000);
+                    client.Show();
+
+                    if (_cancelRequested == 0)
+                        Thread.Sleep(100);
+                }
+            }
+            return 0;
+        }
+    }
+}
